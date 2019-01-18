91f37d10fa04acca26a9d7f1476e8d0d6ca23cce
Andrey <beliy255@gmail.com>
2019-01-18 16:36:32 +0100

    first commit

---
+++ src/java/lectures/lecture1.java
@@ -0,0 +1,5 @@
+package java.lectures;
+
+public class lecture1 {
+    
+}
---
+++ test/java/lectures/lecture1Test.java
@@ -0,0 +1,12 @@
+package java.lectures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class lecture1Test {
+    @Test
+    void name() {
+    }
+}
+
